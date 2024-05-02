18dce709639fd4dc31b311aacc61112c99067f95
barzansaeedpour <barzansaeedpour@gmail.com>
2024-05-02 12:19:14 +0330

    working on producer rabbitmq service

--- client/my_yolo_v8/rabbitmq/publisher.py
+++ client/my_yolo_v8/rabbitmq/publisher.py
@@ -1,60 +1,103 @@
+
 import pika
-import json
-import uuid
+import os
+import time
 
-# Stablish the connection to RabbitMQ
-print("*********************************************")
-def on_open(connection):
-    """Callback when we have connected to the AMQP broker."""
-    print('Connected')
+# read rabbitmq connection url from environment variable
+def publish(plate:str):   
+    pass
+    # read rabbitmq connection url from environment variable
+    # amqp_url = os.environ['AMQP_URL']
+    # url_params = pika.URLParameters(amqp_url)
 
+    # # connect to rabbitmq
+    # connection = pika.BlockingConnection(url_params)
+    # chan = connection.channel()
 
-amqp_url="amqp://rabbitmq?connection_attempts=5&retry_delay=5"
-credentials = pika.PlainCredentials('guest', 'guest')
+    # # declare a new queue
+    # # durable flag is set so that messages are retained
+    # # in the rabbitmq volume even between restarts
+    # chan.queue_declare(queue='hello', durable=True)
 
-parameters = pika.ConnectionParameters('rabbitmq',
-                                       5672,
-                                       '/',
-                                       credentials,
-                                       heartbeat=60
-                                       )
-connection = pika.SelectConnection(parameters, on_open_callback=on_open)
+    # # publish a 100 messages to the queue
+    # for i in range(5):
+    #     chan.basic_publish(exchange='', routing_key='hello',
+    #                     body='Hello World', properties=pika.BasicProperties(delivery_mode=2))
+    #     print("Produced the message")
 
-
-# connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-# connection = pika.BlockingConnection(pika.ConnectionParameters(host='amqp://://rabbitmq:5672'))
-# connection = pika.BlockingConnection(pika.ConnectionParameters(host='0.0.0.0:5762'))
-channel = connection.channel()
-
-# declare the exchange
-channel.exchange_declare(
-    exchange='plate', # name of the exchange
-    exchange_type='direct' # the exchange type
-)
-
-# # the message that we want to send
-# order = {
-#     'id': str(uuid.uuid4()),
-#     'user_email': 'barzansaeedpour@gmail.com',
-#     'product': 'RabbitMQ',
-#     'quantity': 1
-# }
+    # # close the channel and connection
+    # # to avoid program from entering with any lingering
+    # # message in the queue cache
+    # chan.close()
+    # connection.close()
 
 
+#################################################################
+# import pika
+# import json
+# import uuid
+# import os
+# # Stablish the connection to RabbitMQ
+# # print("*********************************************")
+# # def on_open(connection):
+# #     """Callback when we have connected to the AMQP broker."""
+# #     print('Connected')
 
-def publish(plate:str):
-    # publish the messages
-    channel.basic_publish(
-        exchange='plate',  # the exchange that we want to use
-        routing_key='plate.detection', # the routing key
-        body=json.dumps({'plate': plate}) # body of the message
-    )
 
-print('[x] Sent plate message')
+# # amqp_url="amqp://rabbitmq?connection_attempts=5&retry_delay=5"
+# # credentials = pika.PlainCredentials('guest', 'guest')
+
+# # parameters = pika.ConnectionParameters('rabbitmq',
+# #                                        5672,
+# #                                        '/',
+# #                                        credentials,
+# #                                        heartbeat=60
+# #                                        )
+# # connection = pika.SelectConnection(parameters, on_open_callback=on_open)
+
+# # read rabbitmq connection url from environment variable
+# amqp_url = os.environ['AMQP_URL']
+# url_params = pika.URLParameters(amqp_url)
+
+# # connect to rabbitmq
+# connection = pika.BlockingConnection(url_params)
+
+# # connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+# # connection = pika.BlockingConnection(pika.ConnectionParameters(host='amqp://://rabbitmq:5672'))
+# # connection = pika.BlockingConnection(pika.ConnectionParameters(host='0.0.0.0:5762'))
+# channel = connection.channel()
+
+# # declare the exchange
+# channel.exchange_declare(
+#     exchange='plate', # name of the exchange
+#     exchange_type='direct' # the exchange type
+# )
+
+# channel.queue_declare(queue='plate_detection', durable=True)
+
+# # # the message that we want to send
+# # order = {
+# #     'id': str(uuid.uuid4()),
+# #     'user_email': 'barzansaeedpour@gmail.com',
+# #     'product': 'RabbitMQ',
+# #     'quantity': 1
+# # }
 
 
-# close the message
-connection.close() 
+
+# def publish(plate:str):
+#     # publish the messages
+#     channel.basic_publish(
+#         exchange='plate',  # the exchange that we want to use
+#         routing_key='plate.detection', # the routing key
+#         body=json.dumps({'plate': plate}) # body of the message
+#     )
+
+# print('[x] Sent plate message')
+
+
+# # close the message
+# connection.close() 
 
 
 
