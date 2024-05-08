2e3b00911e4ebaa78604841114a9fdb407282771
barzansaeedpour <barzansaeedpour@gmail.com>
2024-05-08 09:14:59 +0330

    working on claims

--- client/app.py
+++ client/app.py
@@ -94,6 +94,13 @@ class Todo(db.Model):
 
     def __repr__(self):
         return '<Task %r' % self.id
+    
+class Claim(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    
+    def __repr__(self):
+        return '<Claim %r' % self.id
 
 
 
