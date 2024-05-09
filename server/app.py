7fa6d607a201d38e640494d19a7142e8726c9669
barzansaeedpour <barzansaeedpour@gmail.com>
2024-05-09 13:01:38 +0330

    working on camera database

--- server/app.py
+++ server/app.py
@@ -108,6 +108,13 @@ class Claim(db.Model):
     
     def __repr__(self):
         return '<Claim %r' % self.id
+    
+class Camera(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200), nullable = False)
+    
+    def __repr__(self):
+        return '<Claim %r' % self.id
 
 
 
