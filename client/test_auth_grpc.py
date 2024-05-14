e068e6063bbfc3af4f405e303b2801d972c00607
barzansaeedpour <barzansaeedpour@gmail.com>
2024-05-14 13:38:46 +0330

    fixed the server for claims request

--- client/test_auth_grpc.py
+++ client/test_auth_grpc.py
@@ -1,22 +1,16 @@
-import claims_pb2
-import claims_pb2_grpc
+import GetServiceClaims_pb2
+import GetServiceClaims_pb2_grpc
+
 import grpc
 
 def get_claims():
     response = []
     with grpc.insecure_channel("localhost:81") as channel:
-        stub = claims_pb2_grpc.ClaimsStub(channel)
-        response = stub.GetClaims(claims_pb2.GetClaimsRequest())
-    print(response.claims)
+        stub = GetServiceClaims_pb2_grpc.GetClaimsStub(channel)
+        response = stub.GetClaimsList(GetServiceClaims_pb2.EmptyRequest())
+    print(response.items)
 
 
 if __name__ == '__main__':
     get_claims()
-
-
-
-
-
-
-
 
