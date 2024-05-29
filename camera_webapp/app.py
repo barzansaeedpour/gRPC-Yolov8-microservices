0756c7742e292553bda1d35d28d051079756d3d1
barzansaeedpour <barzansaeedpour@gmail.com>
2024-05-29 14:48:29 +0330

    working on database connection bug

--- camera_webapp/app.py
+++ camera_webapp/app.py
@@ -261,5 +261,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         fill_database()
-    app.run(debug=debug, host=camera_webapp_address)
+    app.run(debug=debug)
     
